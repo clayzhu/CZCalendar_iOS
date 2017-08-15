4a56e9ef592c061c384e8235fa6816b06f565ad5
Clay Zhu <clayzhu@163.com>
2017-08-16 00:50:20 +0800

    编写获取年月日时分秒的方法。

--- CZCalendarDemo/CZCalendar/NSDate+CZCalendar.h
+++ CZCalendarDemo/CZCalendar/NSDate+CZCalendar.h
@@ -10,4 +10,17 @@
 
 @interface NSDate (CZCalendar)
 
+/** 年 */
+- (NSInteger)year;
+/** 月 */
+- (NSInteger)month;
+/** 日 */
+- (NSInteger)day;
+/** 时 */
+- (NSInteger)hour;
+/** 分 */
+- (NSInteger)minute;
+/** 秒 */
+- (NSInteger)second;
+
 @end
--- CZCalendarDemo/CZCalendar/NSDate+CZCalendar.m
+++ CZCalendarDemo/CZCalendar/NSDate+CZCalendar.m
@@ -10,4 +10,46 @@
 
 @implementation NSDate (CZCalendar)
 
+- (NSInteger)year {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.year;
+}
+
+- (NSInteger)month {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.month;
+}
+
+- (NSInteger)day {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.day;
+}
+
+- (NSInteger)hour {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.hour;
+}
+
+- (NSInteger)minute {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.minute;
+}
+
+- (NSInteger)second {
+    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
+    return dateComponent.second;
+}
+
 @end
