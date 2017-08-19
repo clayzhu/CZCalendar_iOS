79de8ece2adddc38666ca176422385ca6e236462
Clay Zhu <clayzhu@163.com>
2017-08-19 21:28:50 +0800

    添加绘制日历所需的一些方法。

--- CZCalendarDemo/CZCalendar/NSDate+CZCalendar.h
+++ CZCalendarDemo/CZCalendar/NSDate+CZCalendar.h
@@ -8,7 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+@class DayToShowModel;
+
 @interface NSDate (CZCalendar)
+
+/** 年月日时分秒 */
+- (NSDateComponents *)dateComponents;
 
 /** 年 */
 - (NSInteger)year;
@@ -25,11 +30,42 @@
 /** 周几，从1-周日开始 */
 - (NSInteger)weekday;
 
-/** 获取 NSDate 对应月份的天数 */
+/** 获取 self 对应月份的天数 */
 - (NSInteger)numberOfDaysInMonth;
-/** 获取 NSDate 对应月份的第一天 */
+/** 获取 self 对应月份的第一天 */
 - (NSDate *)firstDayOfMonth;
-/** 获取 NSDate 对应月份的第一天是周几，从1-周日开始 */
+/** 获取 self 对应月份的第一天是周几，从1-周日开始 */
 - (NSInteger)firstWeekdayOfMonth;
+
+/**
+ 以 self 对应的月份为基准，计算相邻的月份的 NSDate 数据
+
+ @param offsetNumber 月份偏移量，以 self 对应的月份为0，负数表示计算过去的月份，正数表示计算未来的月份
+ @return 相邻的月份的 NSDate 数据
+ */
+- (NSDate *)adjacentMonthWithOffsetNumber:(NSInteger)offsetNumber;
+/** 计算 self 对应月份的日子，显示日历的一页，以周日开头按星期排序，包括：上个月的最后几天、这个月的日子、下个月的开始几天 */
+- (NSArray<DayToShowModel *> *)daysInMonthToShowForCalendar;
+
+@end
+
+/**
+ 显示在日历的一页中的日子的时期
+
+ - DayToShowPeriodLastMonth: 上个月的最后几天
+ - DayToShowPeriodThisMonth: 这个月的日子
+ - DayToShowPeriodNextMonth: 下个月的开始几天
+ */
+typedef NS_ENUM(NSUInteger, DayToShowPeriod) {
+    DayToShowPeriodLastMonth,
+    DayToShowPeriodThisMonth,
+    DayToShowPeriodNextMonth,
+};
+
+@interface DayToShowModel : NSObject
+
+@property (assign, nonatomic) DayToShowPeriod dayToShowPeriod;
+@property (assign, nonatomic) NSInteger day;
+@property (assign, nonatomic, getter=isToday) BOOL today;
 
 @end
--- CZCalendarDemo/CZCalendar/NSDate+CZCalendar.m
+++ CZCalendarDemo/CZCalendar/NSDate+CZCalendar.m
@@ -10,46 +10,41 @@
 
 @implementation NSDate (CZCalendar)
 
-- (NSInteger)year {
+- (NSDateComponents *)dateComponents {
     NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
     NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.year;
+    NSDateComponents *dateComponents = [calendar components:unitFlags fromDate:self];
+    return dateComponents;
+}
+
+- (NSInteger)year {
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.year;
 }
 
 - (NSInteger)month {
-    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.month;
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.month;
 }
 
 - (NSInteger)day {
-    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.day;
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.day;
 }
 
 - (NSInteger)hour {
-    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.hour;
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.hour;
 }
 
 - (NSInteger)minute {
-    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.minute;
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.minute;
 }
 
 - (NSInteger)second {
-    NSUInteger unitFlags = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *dateComponent = [calendar components:unitFlags fromDate:self];
-    return dateComponent.second;
+    NSDateComponents *dateComponents = [self dateComponents];
+    return dateComponents.second;
 }
 
 - (NSInteger)weekday {
@@ -82,5 +77,80 @@
     NSInteger firstWeekdayOfMonth = [calendar ordinalityOfUnit:NSCalendarUnitWeekday inUnit:NSCalendarUnitWeekOfMonth forDate:firstDayOfMonth];
     return firstWeekdayOfMonth;
 }
+
+- (NSDate *)adjacentMonthWithOffsetNumber:(NSInteger)offsetNumber {
+    if (offsetNumber == 0) {    // 若月份偏移量为0，直接返回基准月份的数据
+        return self;
+    }
+    NSDateComponents *dateComponents = [self dateComponents];
+    NSInteger offsetYear = abs(offsetNumber) / 12;   // 计算月份偏移量是否大于一年
+    NSInteger nowMonth = dateComponents.month;
+    NSInteger offsetMonth = nowMonth + offsetNumber;
+    // 计算偏移后的年份
+    if (offsetMonth <= 0) { // 计算偏移后的月份为0，表示上一年的12月
+        dateComponents.year -= (offsetYear + 1);
+    } else {
+        if (offsetMonth > 12) { // 计算偏移
+            dateComponents.year += (offsetYear + 1);
+        }
+    }
+    // 计算偏移后的月份
+    NSInteger newMonth = (offsetMonth + 12 * (offsetYear + 1)) % 12;
+    dateComponents.month = newMonth == 0 ? 12 : newMonth;
+    // 日子设置为1号
+    dateComponents.day = 1;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDate *newDate = [calendar dateFromComponents:dateComponents];
+    return newDate;
+}
+
+- (NSArray<DayToShowModel *> *)daysInMonthToShowForCalendar {
+    NSMutableArray<DayToShowModel *> *daysToShowMA = [NSMutableArray array];
+    NSInteger numberOfDaysInMonth = [self numberOfDaysInMonth]; // NSDate 对应月份的天数
+    NSInteger firstWeekdayOfMonth = [self firstWeekdayOfMonth]; // NSDate 对应月份的第一天是周几
+    // 需要显示42个日子的情况：该月有31天，且该月第一天是周五；或者，该月有30或31天，且第一天是周六；需要显示28个日子的情况：该月有28天，且该月第一天是周日
+    NSInteger numberOfDaysToShow;
+    if ((numberOfDaysInMonth == 31 && firstWeekdayOfMonth == 6) || ((numberOfDaysInMonth >= 30) && firstWeekdayOfMonth == 7)) {
+        numberOfDaysToShow = 42;
+    } else if (numberOfDaysInMonth == 28 && firstWeekdayOfMonth == 1) {
+        numberOfDaysToShow = 28;
+    } else {
+        numberOfDaysToShow = 35;
+    }
+    // 补全1号前的日子（上个月的最后几天）
+    NSDate *lastMonthDate = [self adjacentMonthWithOffsetNumber:-1];
+    NSInteger numberOfDaysInLastMonth = [lastMonthDate numberOfDaysInMonth]; // 上个月的天数
+    for (NSInteger i = numberOfDaysInLastMonth - (firstWeekdayOfMonth - 1); i < numberOfDaysInLastMonth; i ++) {
+        DayToShowModel *model = [[DayToShowModel alloc] init];
+        model.dayToShowPeriod = DayToShowPeriodLastMonth;
+        model.day = i + 1;
+        [daysToShowMA addObject:model];
+    }
+    // 该月的日子
+    NSDateComponents *thisDateComponents = [self dateComponents];
+    NSDateComponents *nowDateComponents = [[NSDate date] dateComponents];
+    for (NSInteger j = 0; j < numberOfDaysInMonth; j ++) {
+        DayToShowModel *model = [[DayToShowModel alloc] init];
+        model.dayToShowPeriod = DayToShowPeriodThisMonth;
+        model.day = j + 1;
+        if (thisDateComponents.year == nowDateComponents.year && thisDateComponents.month == nowDateComponents.month && nowDateComponents.day == j + 1) {   // self 对应的月份正好是当月，则寻找到今天的日子
+            model.today = YES;
+        }
+        [daysToShowMA addObject:model];
+    }
+    // 提前补充下个月的开始几天
+    NSInteger numberOfDaysInNextMonthBeginning = numberOfDaysToShow - numberOfDaysInMonth - (firstWeekdayOfMonth - 1);
+    for (NSInteger k = 0; k < numberOfDaysInNextMonthBeginning; k ++) {
+        DayToShowModel *model = [[DayToShowModel alloc] init];
+        model.dayToShowPeriod = DayToShowPeriodNextMonth;
+        model.day = k + 1;
+        [daysToShowMA addObject:model];
+    }
+    return daysToShowMA;
+}
+
+@end
+
+@implementation DayToShowModel
 
 @end
